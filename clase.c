d175c99f060df5a73a971f372fec600af0274141
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-20 15:05:33 -0500

    Tercer commit funcion calcular promedio por valor

--- clase.c
+++ clase.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+
+float calcularPromedio(float arregloNotas[], int tamano) {
+   float suma = 0.0; 
+   for (int i = 0; i < tamano; i++) {
+       printf("notas[%d] %.1f\n", i, arregloNotas[i]);
+       suma += arregloNotas[i]; 
+   }
+   return suma / tamano; 
+}
 int main (int argc, char *argv[]) {
 
    float notas[5] = {6.5, 7.1, 8, 6, 5};
    
-   float suma = 0.0;
-   for (int i = 0; i < 5; i++){
-       printf("notas[%d] %.1f\n", i, notas[i]);
-       suma += notas[i];
-   }
-   float promedio = suma / 5.0f;
+   float promedio = calcularPromedio(notas, 5);
+  
    printf("promedio: %.2f\n", promedio);
    return 0;
 }
