065379d52f2c3dc2de9f9d8d1b878f7ba1a1a4cb
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-20 15:11:23 -0500

    Cuarto commit funcion calcular por referencia

--- clase.c
+++ clase.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-float calcularPromedio(float arregloNotas[], int tamano) {
-   float suma = 0.0; 
-   for (int i = 0; i < tamano; i++) {
-       printf("notas[%d] %.1f\n", i, arregloNotas[i]);
-       suma += arregloNotas[i]; 
-   }
-   return suma / tamano; 
-}
+//float calcularPromedioxRef(float *ptr);
 int main (int argc, char *argv[]) {
-
+   
    float notas[5] = {6.5, 7.1, 8, 6, 5};
    
-   float promedio = calcularPromedio(notas, 5);
-  
-   printf("promedio: %.2f\n", promedio);
+   printf("promedio: %.2f\n", calcularPromedioxRef(notas));
    return 0;
+}
+
+float calcularPromedioxRef(float *ptr) {
+   float suma = 0; 
+   for (int i = 0; i < 5; i++) {
+       printf("notas[%d] %.1f\n", i, ptr[i]); 
+       suma += ptr[i];
+   }
+   float promedio = suma / 5.0f;
+   return promedio;
 }
